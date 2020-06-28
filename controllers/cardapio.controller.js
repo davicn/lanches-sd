890e81bcf6cadcc59fc26c5ee93578bbdcb938ac
Davi Nascimento <davi.nascimento777@gmail.com>
2020-06-28 17:28:27 -0300

    Término de endpoits para cardápio

--- controllers/cardapio.controller.js
+++ controllers/cardapio.controller.js
@@ -8,10 +8,11 @@ exports.listAll = async (req, res) => {
     res.status(200).send(response.rows)
 }
 
+// Cadastrar um novo item no cardápio
 exports.createItem = async (req, res) => {
     const { descricao } = req.body
     const { rows } = await db.query(
-        "insert into cardapio (descricao) values ($1)",[descricao]
+        "insert into cardapio (descricao) values ($1)", [descricao]
     )
     res.status(201).send({
         msg: "Novo lanche inserido!",
@@ -21,4 +22,12 @@ exports.createItem = async (req, res) => {
             }
         }
     })
+}
+
+// Excluir um item do cardápio
+exports.deleteitem = async (req, res) => {
+    const idItem = parseInt(req.params.id)
+    await db.query("delete from cardapio where idCardapio=$1", [idItem])
+
+    res.status(200).send({ message: 'Item deleta do cardápio!', idItem });
 }
--- routes/cardapio.routes.js
+++ routes/cardapio.routes.js
@@ -1,8 +1,13 @@
 const router = require('express-promise-router')()
 const cardapioController = require('../controllers/cardapio.controller')
 
-
+// Listar o cardápio de lanches
 router.get('/cardapio',cardapioController.listAll)
+
+// Cadastrar um novo item no cardápio
 router.post('/cardapio',cardapioController.createItem)
+
+// Excluir um item do cardápio
+router.delete('/cardapio/:id',cardapioController.deleteitem)
 
 module.exports = router

5a486f0d51692376fe85a3643cb49eaa5640b64f
Davi Nascimento <davi.nascimento777@gmail.com>
2020-06-28 21:06:27 -0300

    Mudança em conexões do banco

--- database/config.js
+++ database/config.js
@@ -1,14 +1,18 @@
 const { Pool } = require('pg')
-//const dotenv = require('dotenv')
+const dotenv = require('dotenv')
 
-//dotenv.config()
+dotenv.config()
 
 const pool = new Pool({
-    user: 'postgres',
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+    /*user: 'postgres',
     host: 'localhost',
     database: 'db_lanches',
     password: '12345',
-    port: 5432,
+    port: 5432,*/
     //conString: "postgres://postgres:12345@localhost:5432/db_lanche"//process.env.DATABASE_URL
 })
 
